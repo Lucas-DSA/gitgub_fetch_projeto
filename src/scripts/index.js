d94cb5db95c402c81b1001260c845ca667e9d52e
Lucas Alves <lucasalvespdq@hotmail.com>
2024-07-16 16:52:10 -0300

    Primeira aula do projeto

---
+++ src/scripts/index.js
@@ -0,0 +1,23 @@
+document.getElementById('btn-search').addEventListener('click', () => {
+    const userName = document.getElementById('input-search').value
+    getUserProfile(userName)
+})
+
+async function user(userName) {
+    const response = await fetch(`https://api.github.com/users/${userName}`)
+    return await response.json()
+}
+
+function getUserProfile(userName) {
+    user(userName).then(userData => {
+        console.log(userData)
+         let userInfo = `<img src="${userData.avatar_url}" alt="Foto do Perfil do usuario" />
+                        <div class="data">
+                            <h1>${userData.name ?? 'Não possui nome cadastrado 😢'}</h1>
+                            <p>${userData.bio ?? 'Não possui bio cadastrada 😢'}</p>
+                        </div>`
+
+        document.querySelector('.profile-data').innerHTML = userInfo
+    })
+}
+
